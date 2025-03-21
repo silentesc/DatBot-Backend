2ff65b301d24e4b4b501405107d2b3101ee61a01
silentesc <scope-frisk-copy@duck.com>
2025-03-21 18:57:14 +0100

    Add database schema creation for reaction role functionality and improve parameter handling in DbManager

--- src/utils/db_manager.py
+++ src/utils/db_manager.py
@@ -9,21 +9,53 @@ class DbManager:
 
 
     def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.commit()
         self.conn.close()
 
 
-    def execute(self, query: str, params: tuple | None):
+    def execute(self, query: str, params: tuple = ()):
         with self.conn:
-            self.conn.execute(query, params or ())
+            self.conn.execute(query, params)
 
 
-    def execute_fetchall(self, query: str, params: tuple | None):
+    def execute_fetchall(self, query: str, params: tuple = ()):
         with self.conn:
-            cursor = self.conn.execute(query, params or ())
+            cursor = self.conn.execute(query, params)
         return cursor.fetchall()
 
 
-    def execute_fetchone(self, query: str, params: tuple | None):
+    def execute_fetchone(self, query: str, params: tuple = ()):
         with self.conn:
-            cursor = self.conn.execute(query, params or ())
+            cursor = self.conn.execute(query, params)
         return cursor.fetchone()
+
+
+if __name__ == "__main__":
+    with DbManager() as db:
+        db.execute(query="""
+            CREATE TABLE reaction_role_emoji_roles (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                emoji IDK,
+                dc_role_id VARCHAR(255)
+            )
+            """
+        )
+        
+        db.execute(query="""
+            CREATE TABLE reaction_role_messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                db_guild_id VARCHAR(255),
+                db_role_id VARCHAR(255),
+                message TEXT
+            )
+            """
+        )
+
+        db.execute(query="""
+            CREATE TABLE reaction_roles (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                reaction_role_messages_id INTEGER,
+                reaction_role_emoji_roles_id INTEGER
+            )
+            """
+        )
