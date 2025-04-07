df5807268fc4b537b61907d3e3018f3bcc4901a3
silentesc <scope-frisk-copy@duck.com>
2025-04-07 21:40:48 +0200

    Add middleware to log client IP addresses in public app

--- run.py
+++ run.py
@@ -1,9 +1,22 @@
 import uvicorn
 import asyncio
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from loguru import logger
 
 from src.public_app import app as public_app
 from src.internal_app import app as internal_app
+
+
+@public_app.middleware("http")
+async def log_client_ip(request: Request, call_next):
+    x_forwarded_for = request.headers.get("X-Forwarded-For")
+    if x_forwarded_for:
+        client_ip = x_forwarded_for.split(",")[0].strip()
+    else:
+        client_ip = request.client.host
+    logger.info(f"Request from client: {client_ip} - {request.method} {request.url}")
+    response = await call_next(request)
+    return response
 
 
 async def run_app(app: FastAPI, host: str, port: int):
